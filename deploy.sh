d7f93f200e5a02fd98b59d19bf860d542910eb86
HelloWorldYC <1228145722@qq.com>
2024-08-11 22:02:44 +0800

    docs: 更新 deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,17 +7,8 @@ set -e
 
 echo '开始执行命令'
 
-# 源文件发布到 blog-myc 仓库的 master 分支
-echo '执行命令：git add -A'
-git add -A
-echo "git commit -m "
-git commit -m "update blog on $var1"
-echo '执行命令：git push -f git@github.com:HelloWorldYC/blog-myc.git master'
-git push -f git@github.com:HelloWorldYC/blog-myc.git master
-
-
 # 进入 public 文件夹
-echo "执行命令：cd ./docs/.vuepress/dist\n"
+echo "执行命令：cd ./public"
 cd ./public
 
 # 把网页文件发布到 blog-myc 仓库的 gh-page 分支
